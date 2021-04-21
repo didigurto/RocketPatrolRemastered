68a437f0172aa1e83c9681f682414bb8683daa7c
Your Name <student@ucsc.edu>
2021-04-21 11:15:18 -0700

    adding comments for canvas submission

--- src/main.js
+++ src/main.js
@@ -12,3 +12,13 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize/3 ;
 
 let keyLEFT, keyRIGHT, keyF, keyR; 
+
+
+// Diego Agurto, Rocket Patrol Remastered, 4/21, 
+// So far, I have worked on the project for about 8 hours
+//Point Breakdown so far:
+//   - Create new scrolling tile sprite for the background (5)
+//   - Create new artwork for all in-game assets(rocket, spaceships, [explosions]) (20)
+//   - Eventual additions:
+//      - Redesign the game's artwork, UI, and sound to change its theme/aesthetic (60)
+//      - Create and implement a new weapon (20)
